03194edb1248d2c7bcc5ff0ab07f721c6d3d05bb
Jummi10 <98qkrwjdal@naver.com>
2024-05-19 22:57:29 +0900

    chore: React import 추가

--- App.js
+++ App.js
@@ -3,13 +3,16 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Splash from "./pages/Splash";
 import Main from "./pages/Main";
-
+import React from "react";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
-
-  return (<NavigationContainer>
-      <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Splash"
+        screenOptions={{ headerShown: false }}
+      >
         <Stack.Screen name="Splash" component={Splash} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Main" component={Main} />
